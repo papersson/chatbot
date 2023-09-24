e929ec47ded0c9741a09be4a9bb64547f0c24d63
ElGutto <patrik95195@gmail.com>
2023-09-24 13:08:09 +0200

    clean up

--- backend/config/utils.py
+++ backend/config/utils.py
@@ -3,10 +3,12 @@ from azure.keyvault.secrets import SecretClient
 
 def get_azure_secret():
     credential = DefaultAzureCredential()
-    secret_client = SecretClient(vault_url="https://kv-gde-main-dev.vault.azure.net/", credential=credential)
+    # secret_client = SecretClient(vault_url="https://kv-gde-main-dev.vault.azure.net/", credential=credential)
 
-    AZURE_OPENAI_ENDPOINT = secret_client.get_secret("OpenAIEastUSEndpoint").value
-    AZURE_OPENAI_KEY = secret_client.get_secret("OpenAIEastUSKey").value
+    # AZURE_OPENAI_ENDPOINT = secret_client.get_secret("OpenAIEastUSEndpoint").value
+    # AZURE_OPENAI_KEY = secret_client.get_secret("OpenAIEastUSKey").value
+    AZURE_OPENAI_ENDPOINT = "https://oai-gpt4-qna-poc.openai.azure.com"
+    AZURE_OPENAI_KEY = "595ea12c831c4b89aaa04ca80acdd156"
 
 
     print("Retrieved Azure secret.")
